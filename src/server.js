87951306b9d33ced727b188384afe6fa01fbf7ed
UmamiAppearance <mail@umamiappearance.eu>
2022-05-26 09:39:35 +0200

    version udpate

--- src/server.js
+++ src/server.js
@@ -1,7 +1,7 @@
 /*
  * [NoBroCote|HTML Server]{@link https://github.com/UmamiAppearance/NoBroCote}
  *
- * @version 0.1.1
+ * @version 0.1.2
  * @author UmamiAppearance [mail@umamiappearance.eu]
  * @license GPL-3.0
  */

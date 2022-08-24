e714da8f88ab88267ef80a1bbb92e36af92ea77f
UmamiAppearance <mail@umamiappearance.eu>
2022-08-24 13:09:43 +0200

    v0.2.1

--- src/server.js
+++ src/server.js
@@ -1,7 +1,7 @@
 /*
  * [NoBroCote|HTML Server]{@link https://github.com/UmamiAppearance/NoBroCote}
  *
- * @version 0.2.0
+ * @version 0.2.1
  * @author UmamiAppearance [mail@umamiappearance.eu]
  * @license GPL-3.0
  */

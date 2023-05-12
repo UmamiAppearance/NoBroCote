714dc601419bd1ebb2563257a3bfeb50d7cf8ab2
UmamiAppearance <mail@umamiappearance.eu>
2023-05-12 21:31:38 +0200

    v0.2.16

--- src/no-bro-cote.js
+++ src/no-bro-cote.js
@@ -1,7 +1,7 @@
 /**
  * [NoBroCote]{@link https://github.com/UmamiAppearance/NoBroCote}
  *
- * @version 0.2.15
+ * @version 0.2.16
  * @author UmamiAppearance [mail@umamiappearance.eu]
  * @license GPL-3.0
  */

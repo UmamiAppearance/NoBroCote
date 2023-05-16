f2096f6ef535acc3dd177e523702122cba51e0e6
UmamiAppearance <mail@umamiappearance.eu>
2023-05-16 12:43:41 +0200

    dependency updates

--- src/no-bro-cote.js
+++ src/no-bro-cote.js
@@ -1,7 +1,7 @@
 /**
  * [NoBroCote]{@link https://github.com/UmamiAppearance/NoBroCote}
  *
- * @version 0.2.16
+ * @version 0.2.17
  * @author UmamiAppearance [mail@umamiappearance.eu]
  * @license GPL-3.0
  */
